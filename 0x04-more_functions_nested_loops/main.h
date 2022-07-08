4a7f5c3668d6f32a55c123d998cf99a328c7b750
LEYLANK <natashaleilak@gmail.com>
2022-07-08 00:05:28 -0700

    S

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,8 +8,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_diagonal(int n)
-
-
-
+void print_square(int size);
+void print_triangle(int size);
 	
 #endif
