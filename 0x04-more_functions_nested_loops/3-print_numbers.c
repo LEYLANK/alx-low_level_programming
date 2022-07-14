8f6d7750628a60980f25503602d716d8b459969a
LEYLANK <natashaleilak@gmail.com>
2022-07-14 11:31:05 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,9 @@
 
 void print_numbers(void)
 {
-	int n;
+	char c;
 
-	for (n = 0; n <= 9; n++)
-	{
-		_putchar((n % 10) + '0');
-	}
+	for (c = '0'; c <= '9'; c++)
+	_putchar(c);
 	_putchar('\n');
 }
