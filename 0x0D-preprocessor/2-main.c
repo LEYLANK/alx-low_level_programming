6d34ee306de5b8bb3abca660e9cda1a0549865f9
LEYLANK <natashaleilak@gmail.com>
2022-08-02 09:32:07 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the file name
+ * main - print file name
+ * Return: 0
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
