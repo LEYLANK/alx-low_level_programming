0456bd019246d68bc16587b354a0313105118536
LEYLANK <natashaleilak@gmail.com>
2022-08-04 02:52:40 -0700

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,21 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int index, sum = 0;
-	int sum = 0;
 
-	if (n != 0)
-	{
-		va_start(args, n);
-
-		while (i < n)
-		{
-			sum += va_arg(args, int);
-			i++;
-		}
-
-		va_end(args);
-		return (sum);
-	}
-
-	return (0);
+	if (n == 0)
+		return (0);
+	va_start(nums, n);
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+	va_end(nums);
+	return (sum);
 }
